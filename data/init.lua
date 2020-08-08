0462872ed020fdc63f0b470bfbc8cbd10edaf294
Vladyslav Hrytsenko <vladgritsenko@gmail.com>
2020-08-08 13:35:44 +0300

    changed spaces

--- data/init.lua
+++ data/init.lua
@@ -6,9 +6,9 @@ cols = require "collisions"
 dofile("utils.lua")
 
 config = {
-  hostPort = "8000",
-  host = "lab.zakto.pw",
-  port = "8000"
+    hostPort = "8000",
+    host = "lab.zakto.pw",
+    port = "8000"
 }
 
 -- Modules
@@ -30,9 +30,9 @@ screenRT = RenderTarget(res[1], res[2])
 local aspect = res[1] / res[2]
 local hAdd = aspect * 150.0
 invTexSize = Vector3(
-  1.0 / res[1],
-  1.0 / (res[2] + hAdd),
-  0.0
+    1.0 / res[1],
+    1.0 / (res[2] + hAdd),
+    0.0
 )
 
 fxaaShader = Effect("fx/fxaa.fx")
@@ -49,56 +49,56 @@ RegisterFontFile("assets/slkscr.ttf")
 
 -- Set up nativedllwork update event
 nativedll.setUpdate(function (entity_id, x, y, z, r, c, islocal, serverTrail)
-  if state:is("connecting") then
-    state:switch("game")
-  end
-
-  if islocal == 1 then
-    if serverTrail ~= nil then
-      tanks[-1].serverTrail = serverTrail
+    if state:is("connecting") then
+        state:switch("game")
     end
-    return
-  end
 
-  if tanks[entity_id] == nil then
-    tanks[entity_id] = Tank(entity_id, c)
-    tanks[entity_id].pos = Vector3(x, y, z)
-  end
+    if islocal == 1 then
+        if serverTrail ~= nil then
+            tanks[-1].serverTrail = serverTrail
+        end
+        return
+    end
 
-  local tank = tanks[entity_id]
+    if tanks[entity_id] == nil then
+        tanks[entity_id] = Tank(entity_id, c)
+        tanks[entity_id].pos = Vector3(x, y, z)
+    end
 
-  tank.color = c
-  -- tank.pos = Vector3(x,y,z)
-  local nx = lerp(tank.pos:x(), x, 0.1)
-  local ny = lerp(tank.pos:y(), y, 0.1)
-  local nz = lerp(tank.pos:z(), z, 0.1)
-  tank.pos = Vector3(nx, ny, nz)
-  tank.rot = Matrix():rotate(r+math.rad(90),0,0)
-  tank.aliveTime = getTime() + 5
-  tank.heading = r
+    local tank = tanks[entity_id]
 
-  if serverTrail ~= nil then
-    tank.serverTrail = serverTrail
-  end
-  tank:updateTrail()
+    tank.color = c
+    -- tank.pos = Vector3(x,y,z)
+    local nx = lerp(tank.pos:x(), x, 0.1)
+    local ny = lerp(tank.pos:y(), y, 0.1)
+    local nz = lerp(tank.pos:z(), z, 0.1)
+    tank.pos = Vector3(nx, ny, nz)
+    tank.rot = Matrix():rotate(r+math.rad(90),0,0)
+    tank.aliveTime = getTime() + 5
+    tank.heading = r
+
+    if serverTrail ~= nil then
+        tank.serverTrail = serverTrail
+    end
+    tank:updateTrail()
 end)
 
 nativedll.setCollide(function(killer_id, victim_id)
-  -- ignore state switch when paused
-  if not state:is("game") then
-    return
-  end
-
-  if victim_id == -1 then
-    state:switch("death")
-    LogString("BOOM WE GOT KILLED BY " .. killer_id)
-  else
-    if tanks[victim_id] ~= nil then
-      LogString("Removing PLAYERS TRAIL KILLING IT AND STUFF")
-      tanks[victim_id].alive = false
-      tanks[victim_id].trails = {}
+    -- ignore state switch when paused
+    if not state:is("game") then
+        return
     end
-  end
+
+    if victim_id == -1 then
+        state:switch("death")
+        LogString("BOOM WE GOT KILLED BY " .. killer_id)
+    else
+        if tanks[victim_id] ~= nil then
+            LogString("Removing PLAYERS TRAIL KILLING IT AND STUFF")
+            tanks[victim_id].alive = false
+            tanks[victim_id].trails = {}
+        end
+    end
 end)
 
 nativedll.setRespawn(function(entity_id)
@@ -119,74 +119,74 @@ state:switch("menu")
 ui.init()
 
 function _destroy()
-  SaveState(encode(config))
-   nativedll.disconnect()
-   nativedll.serverStop()
+    SaveState(encode(config))
+    nativedll.disconnect()
+    nativedll.serverStop()
 end
 
 function _update(dt)
-  nativedll.update()
-  music:update(dt)
+    nativedll.update()
+    music:update(dt)
 
-  if GetKey(KEY_CONTROL) and GetKeyDown("R") then
-      RestartGame()
-  end
+    if GetKey(KEY_CONTROL) and GetKeyDown("R") then
+        RestartGame()
+    end
 
-  if IsFocused() and not state.showingCursor then
-    ShowCursor(false)
-    SetCursorMode(CURSORMODE_CENTERED)
-  else
-    ShowCursor(true)
-    SetCursorMode(CURSORMODE_DEFAULT)
-  end
+    if IsFocused() and not state.showingCursor then
+        ShowCursor(false)
+        SetCursorMode(CURSORMODE_CENTERED)
+    else
+        ShowCursor(true)
+        SetCursorMode(CURSORMODE_DEFAULT)
+    end
 
-  for _, t in pairs(tanks) do
-    t:update(dt)
-  end
+    for _, t in pairs(tanks) do
+        t:update(dt)
+    end
 
-  localPlayer:update(dt)
+    localPlayer:update(dt)
 
-  time = time + dt
+    time = time + dt
 
-  ui.update()
-  state:update(dt)
+    ui.update()
+    state:update(dt)
 end
 
 function _charInput(key)
-  ui.input(key)
-  state:input(key)
+    ui.input(key)
+    state:input(key)
 end
 
 function _render()
-  screenRT:bind()
-  EnableLighting(true)
-  ClearScene(15,0,15)
-  AmbientColor(0x773377)
-  CameraPerspective(75, 1.0, 25000)
-  Matrix():bind(WORLD)
-  localPlayer.cam:bind(VIEW)
-  world:draw()
-  for _, t in pairs(tanks) do
-    t:draw()
-  end
+    screenRT:bind()
+    EnableLighting(true)
+    ClearScene(15,0,15)
+    AmbientColor(0x773377)
+    CameraPerspective(75, 1.0, 25000)
+    Matrix():bind(WORLD)
+    localPlayer.cam:bind(VIEW)
+    world:draw()
+    for _, t in pairs(tanks) do
+        t:draw()
+    end
 
-  local wmat = Matrix():scale(WORLD_TILES[1], WORLD_TILES[1], WORLD_TILES[1])
-  CullMode(CULLKIND_NONE)
-  world.boundsMesh:draw(wmat)
-  CullMode(CULLKIND_CCW)
-  ClearTarget()
+    local wmat = Matrix():scale(WORLD_TILES[1], WORLD_TILES[1], WORLD_TILES[1])
+    CullMode(CULLKIND_NONE)
+    world.boundsMesh:draw(wmat)
+    CullMode(CULLKIND_CCW)
+    ClearTarget()
 
-  drawEffect(fxaaShader, "FXAA", function (fx)
-    fx:setTexture("srcTex", screenRT)
-    fx:setVector3("inverseTexSize", invTexSize)
-		fx:setFloat("fxaaReduceMul", 1.0 / 8.0)
-		fx:setFloat("fxaaReduceMin", 1.0 / 128.0)
-    fx:setFloat("fxaaSpanMax", 8.0)
-    FillScreen()
-  end)
+    drawEffect(fxaaShader, "FXAA", function (fx)
+        fx:setTexture("srcTex", screenRT)
+        fx:setVector3("inverseTexSize", invTexSize)
+        fx:setFloat("fxaaReduceMul", 1.0 / 8.0)
+        fx:setFloat("fxaaReduceMin", 1.0 / 128.0)
+        fx:setFloat("fxaaSpanMax", 8.0)
+        FillScreen()
+    end)
 end
 
 function _render2d()
-  state:draw2d()
-  music:draw2d()
+    state:draw2d()
+    music:draw2d()
 end
--- data/utils.lua
+++ data/utils.lua
@@ -5,37 +5,37 @@ local b1, b2 =  sqrt( 3 )/2, -0.5
 
 
 function HSVToRGB( h, s, v, a )
-  h=h+math.pi/2--because the r vector is up
-  local r, g, b = 1, 1, 1
-  local h1, h2 = cos( h ), sin( h )
+    h=h+math.pi/2--because the r vector is up
+    local r, g, b = 1, 1, 1
+    local h1, h2 = cos( h ), sin( h )
 
-  --hue
-  r = h1*r1 + h2*r2
-  g = h1*g1 + h2*g2
-  b = h1*b1 + h2*b2
-  --saturation
-  r = r + (1-r)*s
-  g = g + (1-g)*s
-  b = b + (1-b)*s
+    --hue
+    r = h1*r1 + h2*r2
+    g = h1*g1 + h2*g2
+    b = h1*b1 + h2*b2
+    --saturation
+    r = r + (1-r)*s
+    g = g + (1-g)*s
+    b = b + (1-b)*s
 
-  r,g,b = r*v, g*v, b*v
+    r,g,b = r*v, g*v, b*v
 
-  return r*255, g*255, b*255, (a or 1) * 255
+    return r*255, g*255, b*255, (a or 1) * 255
 end
 
 function dump(o)
-   if type(o) == 'table' then
-      local s = '{ '
-      for k,v in pairs(o) do
-         if type(k) ~= 'number' then k = '"'..k..'"' end
-         s = s .. '['..k..'] = ' .. dump(v) .. ','
-      end
-      return s .. '} '
-   else
-      return tostring(o)
-   end
+     if type(o) == 'table' then
+            local s = '{ '
+            for k,v in pairs(o) do
+                 if type(k) ~= 'number' then k = '"'..k..'"' end
+                 s = s .. '['..k..'] = ' .. dump(v) .. ','
+            end
+            return s .. '} '
+     else
+            return tostring(o)
+     end
 end
 
 function lerp(v0, v1, t)
-   return v0 + t * (v1 - v0);
+     return v0 + t * (v1 - v0);
  end
